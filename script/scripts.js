3db70edf313475a4926855e770af683cc1095e53
Mprds <83730014+Mprds@users.noreply.github.com>
2022-02-05 16:25:54 -0300

    script

--- script/scripts.js
+++ script/scripts.js
@@ -1,6 +1,13 @@
-body{
-    display:fle;
-    text-align:center;
-    align-itens:center;
-    flex-direction:column
+function calcular(n1,n2){
+   n1 = document.getElementsByid("n1").value
+   n2 = document.getElementsByid("n2").value
+
+   seletor = document.getElementById("selector").value
+
+   switch(selector){
+       case "+":
+           calculo = (n1+n2)
+           document.getElementById("resultado").innerHTML ='O resultado da doma de ${n1} + ${n2} é = ${calculo}'
+
+   }
 }
