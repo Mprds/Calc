614b5ae2afdc7db7700688e36d8bc5d37c866ee8
Mprds <83730014+Mprds@users.noreply.github.com>
2022-02-05 19:27:59 -0300

    nova tentativa de calculo

--- script/scripts.js
+++ script/scripts.js
@@ -1,13 +1,12 @@
-function calcular(n1,n2){
-   n1 = document.getElementsByid("n1").value
-   n2 = document.getElementsByid("n2").value
+function calcular(n1, n2) {
+    n1 = document.getElementById("n1").value
+    n2 = document.getElementById("N2").value
 
-   seletor = document.getElementById("selector").value
+    selector = document.getElementById("selecor").value
 
-   switch(selector){
-       case "+":
-           calculo = (n1+n2)
-           document.getElementById("resultado").innerHTML ='O resultado da doma de ${n1} + ${n2} é = ${calculo}'
-
-   }
+    switch (selector) {
+        case "+":
+            calculo = (n1 + n2)
+            document.getElementById("resultado").innerHTML - "o resultado da soma de $(n1)+(n2) é - $(calculo)"
+    }
 }
