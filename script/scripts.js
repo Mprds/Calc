3331dfeab2396b09270efb8db0ebeffd60c2e0ab
Mprds <83730014+Mprds@users.noreply.github.com>
2022-02-06 17:14:58 -0300

    scripit com defeito

--- script/scripts.js
+++ script/scripts.js
@@ -1,12 +1,13 @@
 function calcular(n1, n2) {
     n1 = document.getElementById("n1").value
-    n2 = document.getElementById("N2").value
+    n2 = document.getElementById("n2").value
 
     selector = document.getElementById("selecor").value
 
     switch (selector) {
         case "+":
             calculo = (n1 + n2)
-            document.getElementById("resultado").innerHTML - "o resultado da soma de $(n1)+(n2) é - $(calculo)"
+            document.getElementById("resultado").innerHTML = "o resultado da soma de $(n1)+$(n2) é =$(n2)'
+
     }
 }
